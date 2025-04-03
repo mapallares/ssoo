3427c62fd502afc0f3e92e8bc652b43c4b718e9b
Martín Pallares <mapallares@unimagdalena.edu.co>
2025-04-02 19:48:33 -0500

    feat: add lib utility fuctions

--- tone/libs/utils/utils.c
+++ tone/libs/utils/utils.c
@@ -5,7 +5,33 @@ void error(char *msg){
     exit(-1);
 }
 
-int readVectorFromFile(char *filename, int **vector) {
+void allocateVector(int** vector, int size) {
+    *vector = (int*) calloc(size, sizeof(int));
+    if(!*vector) { error("Error allocating memory (calloc) for the vector."); }
+}
+
+void freeVector(int* vector) {
+    free(vector);
+}
+
+void allocateMatrix(int*** matrix, int rows, int cols) {
+    *matrix = (int**) calloc(rows, sizeof(int*));
+    if(!*matrix) { error("Error allocating memory (calloc) for the matrix."); }
+    
+    for (int i = 0; i < rows; i++) {
+        (*matrix)[i] = (int*) calloc(cols, sizeof(int));
+        if (!(*matrix)[i]) { error("Error allocating memory (calloc) for matrix row %d.\n", i) }
+    }
+}
+
+void freeMatrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+void readVectorFromFile(char *filename, int **vector) {
     int c, numero, totalNumeros;
     FILE *infile;
     infile = fopen(filename, "r");
@@ -23,8 +49,6 @@ int readVectorFromFile(char *filename, int **vector) {
     }
 
     fclose(infile);
-
-    return c;  
 }
 
 void writeVectorToFile(char *fileName, int *vector, int size) {
@@ -50,7 +74,7 @@ void printVector(int *vector, int size) {
     printf("\n");
 }
 
-int sumVectorRange(int *vector, int startIndex, int endIndex) {
+int sumVectorElementsInRange(int *vector, int startIndex, int endIndex) {
     int sum = 0;
     for (int i = startIndex; i < endIndex; i++) {
         sum += vector[i];
--- tone/libs/utils/utils.h
+++ tone/libs/utils/utils.h
@@ -11,8 +11,20 @@
 // Función para manejar errores
 void error(char *msg);
 
+// Función para reservar memoria para un vector dinámico
+void allocateVector(int** vector, int size);
+
+// Función para liberar la memoria del vector
+void freeVector(int* vector);
+
+// Función para reservar memoria para una matriz dinámica
+void allocateMatrix(int*** matrix, int rows, int cols);
+
+// Función para liberar la memoria de la matriz
+void freeMatrix(int** matrix, int rows);
+
 // Función para leer un vector desde un archivo
-int readVectorFromFile(char *filename, int **vector);
+void readVectorFromFile(char *filename, int **vector);
 
 // Función para escribir un vector en un archivo
 void writeVectorToFile(char *fileName, int *vector, int size);
@@ -21,6 +33,6 @@ void writeVectorToFile(char *fileName, int *vector, int size);
 void printVector(int *vector, int size);
 
 //Función para sumar los valores de un vector en un rango
-int sumVectorRange(int *vector, int startIndex, int endIndex);
+int sumVectorElementsInRange(int *vector, int startIndex, int endIndex);
 
 #endif
